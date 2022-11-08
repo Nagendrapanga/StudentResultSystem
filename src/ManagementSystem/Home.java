71ea815dbe8b471d24d90b64d2188dc292b51fb7
Nagendrapanga <116372316+Nagendrapanga@users.noreply.github.com>
2022-11-08 11:45:42 +0530

    Update Home.java

--- src/ManagementSystem/Home.java
+++ src/ManagementSystem/Home.java
@@ -5,10 +5,8 @@ public class Home extends javax.swing.JFrame
      {
         initComponents();
      }
-     
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
-
+    private void initComponents() 
+    {
         student = new javax.swing.JButton();
         admin = new javax.swing.JButton();
         background = new javax.swing.JLabel();
@@ -17,18 +15,20 @@ public class Home extends javax.swing.JFrame
         setMinimumSize(new java.awt.Dimension(700, 560));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        student.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        student.setIcon(new javax.swing.ImageIcon("C:\\Users\\Dell\\OneDrive\\Desktop\\project\\StudentResultManagementSystem\\Icon\\icons8-schoolboy-at-a-desk-30.png")); // NOI18N
+        student.setFont(new java.awt.Font("Segoe UI", 1, 14)); 
+        student.setIcon(new javax.swing.ImageIcon("C:\\Users\\Dell\\OneDrive\\Desktop\\project\\StudentResultManagementSystem\\Icon\\icons8-schoolboy-at-a-desk-30.png"));
         student.setText("Student");
-        student.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        student.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt) 
+            {
                 studentActionPerformed(evt);
             }
         });
         getContentPane().add(student, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 330, 120, 50));
 
-        admin.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        admin.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ManagementSystem/icons8-admin-settings-male-30.png"))); // NOI18N
+        admin.setFont(new java.awt.Font("Segoe UI", 1, 14));
+        admin.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ManagementSystem/icons8-admin-settings-male-30.png"))); 
         admin.setText("Admin");
         admin.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -37,26 +37,28 @@ public class Home extends javax.swing.JFrame
         });
         getContentPane().add(admin, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 330, 120, 50));
 
-        background.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ManagementSystem/wp7701735-anime-school-aesthetic-wallpapers.png"))); // NOI18N
+        background.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ManagementSystem/wp7701735-anime-school-aesthetic-wallpapers.png")));
         background.setText("jLabel1");
         getContentPane().add(background, new org.netbeans.lib.awtextra.AbsoluteConstraints(-150, -90, -1, -1));
 
         pack();
-    }// </editor-fold>//GEN-END:initComponents
+    }
 
-    private void adminActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_adminActionPerformed
+    private void adminActionPerformed(java.awt.event.ActionEvent evt) 
+    {
         setVisible(false);
         AdminHomePage frame= new AdminHomePage();
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
-    }//GEN-LAST:event_adminActionPerformed
+    }
 
-    private void studentActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_studentActionPerformed
+    private void studentActionPerformed(java.awt.event.ActionEvent evt) 
+    {
         setVisible(false);
         StudentHomePage frame= new StudentHomePage();
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
-    }//GEN-LAST:event_studentActionPerformed
+    }
 
     public static void main(String args[]) 
     {
@@ -70,10 +72,7 @@ public class Home extends javax.swing.JFrame
             }
         });
     }
-
-    // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton admin;
     private javax.swing.JLabel background;
     private javax.swing.JButton student;
-    // End of variables declaration//GEN-END:variables
 }
