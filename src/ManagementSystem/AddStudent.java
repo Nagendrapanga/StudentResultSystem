d136d5594b4dbe567790223ac4d7c510fc96374d
Nagendrapanga <116372316+Nagendrapanga@users.noreply.github.com>
2022-11-08 11:37:21 +0530

    Update AddStudent.java

--- src/ManagementSystem/AddStudent.java
+++ src/ManagementSystem/AddStudent.java
@@ -9,9 +9,8 @@ public class AddStudent extends javax.swing.JFrame
     {
         initComponents();
     }
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
-
+    private void initComponents() 
+    {
         name = new javax.swing.JLabel();
         rollno = new javax.swing.JLabel();
         gender = new javax.swing.JLabel();
@@ -33,56 +32,58 @@ public class AddStudent extends javax.swing.JFrame
         setMinimumSize(new java.awt.Dimension(700, 560));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        name.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        name.setFont(new java.awt.Font("Segoe UI", 1, 14));
         name.setText("Name");
         name.setMaximumSize(new java.awt.Dimension(180, 70));
         name.setMinimumSize(new java.awt.Dimension(180, 70));
         name.setPreferredSize(new java.awt.Dimension(180, 70));
         getContentPane().add(name, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 20, 120, 40));
 
-        rollno.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        rollno.setFont(new java.awt.Font("Segoe UI", 1, 14)); 
         rollno.setText("Roll No");
         rollno.setMaximumSize(new java.awt.Dimension(180, 70));
         rollno.setMinimumSize(new java.awt.Dimension(180, 70));
         rollno.setPreferredSize(new java.awt.Dimension(180, 70));
         getContentPane().add(rollno, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 70, 100, 40));
 
-        gender.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        gender.setFont(new java.awt.Font("Segoe UI", 1, 14));
         gender.setText("Gender");
         gender.setMaximumSize(new java.awt.Dimension(180, 70));
         gender.setMinimumSize(new java.awt.Dimension(180, 70));
         gender.setPreferredSize(new java.awt.Dimension(180, 70));
         getContentPane().add(gender, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 130, 90, 40));
 
-        fathername.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        fathername.setFont(new java.awt.Font("Segoe UI", 1, 14));
         fathername.setText("Father's Name");
         fathername.setMaximumSize(new java.awt.Dimension(180, 70));
         fathername.setMinimumSize(new java.awt.Dimension(180, 70));
         fathername.setPreferredSize(new java.awt.Dimension(180, 70));
         getContentPane().add(fathername, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 190, 140, 30));
 
-        course.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        course.setFont(new java.awt.Font("Segoe UI", 1, 14));
         course.setText("Course");
         course.setMaximumSize(new java.awt.Dimension(180, 70));
         course.setMinimumSize(new java.awt.Dimension(180, 70));
         course.setPreferredSize(new java.awt.Dimension(180, 70));
         getContentPane().add(course, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 240, 100, 30));
 
-        branch.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        branch.setFont(new java.awt.Font("Segoe UI", 1, 14));
         branch.setText("Branch");
         branch.setMaximumSize(new java.awt.Dimension(180, 70));
         branch.setMinimumSize(new java.awt.Dimension(180, 70));
         branch.setPreferredSize(new java.awt.Dimension(180, 70));
         getContentPane().add(branch, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 290, 100, 30));
 
-        submit.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        submit.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ManagementSystem/icons8-submit-document-32.png"))); // NOI18N
+        submit.setFont(new java.awt.Font("Segoe UI", 1, 14));
+        submit.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ManagementSystem/icons8-submit-document-32.png"))); 
         submit.setText("Submit");
         submit.setMaximumSize(new java.awt.Dimension(180, 70));
         submit.setMinimumSize(new java.awt.Dimension(180, 70));
         submit.setPreferredSize(new java.awt.Dimension(180, 70));
-        submit.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        submit.addActionListener(new java.awt.event.ActionListener() 
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
                 submitActionPerformed(evt);
             }
         });
@@ -101,13 +102,15 @@ public class AddStudent extends javax.swing.JFrame
         });
         getContentPane().add(cancel, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 370, 110, 30));
 
-        clear.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        clear.setFont(new java.awt.Font("Segoe UI", 1, 14));
         clear.setText("Clear");
         clear.setMaximumSize(new java.awt.Dimension(180, 70));
         clear.setMinimumSize(new java.awt.Dimension(180, 70));
         clear.setPreferredSize(new java.awt.Dimension(180, 70));
-        clear.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        clear.addActionListener(new java.awt.event.ActionListener() 
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt) 
+            {
                 clearActionPerformed(evt);
             }
         });
@@ -125,22 +128,23 @@ public class AddStudent extends javax.swing.JFrame
         getContentPane().add(branchofstudent, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 300, 150, 20));
         getContentPane().add(nameoffather, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 190, 150, -1));
 
-        background.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ManagementSystem/wp7701735-anime-school-aesthetic-wallpapers.png"))); // NOI18N
+        background.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ManagementSystem/wp7701735-anime-school-aesthetic-wallpapers.png")));
         background.setText("jLabel7");
         getContentPane().add(background, new org.netbeans.lib.awtextra.AbsoluteConstraints(-170, -80, -1, -1));
 
         pack();
-    }// </editor-fold>//GEN-END:initComponents
+    }
 
-    private void cancelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cancelActionPerformed
+    private void cancelActionPerformed(java.awt.event.ActionEvent evt) {
          int flag = JOptionPane.showConfirmDialog(null,"Are You Sure Do You Want To Exit","Submit",JOptionPane.YES_NO_OPTION);
          if(flag==0)
          {
              setVisible(false);
          }
-    }//GEN-LAST:event_cancelActionPerformed
+    }
 
-    private void submitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_submitActionPerformed
+    private void submitActionPerformed(java.awt.event.ActionEvent evt) 
+    {
         String name = nameofthestudent.getText();
         String rollNo = rollnoforstudent.getText();
         String gender = (String)genderofstudent.getSelectedItem();
@@ -163,14 +167,15 @@ public class AddStudent extends javax.swing.JFrame
         {
              JOptionPane.showMessageDialog(null,e.toString());
         }
-    }//GEN-LAST:event_submitActionPerformed
+    }
 
-    private void clearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_clearActionPerformed
+    private void clearActionPerformed(java.awt.event.ActionEvent evt) 
+    {
             setVisible(false);
             AddStudent frame = new AddStudent();
             frame.setVisible(true);
             frame.setLocationRelativeTo(null);  
-    }//GEN-LAST:event_clearActionPerformed
+    }
         public static void main(String args[])
         {
         java.awt.EventQueue.invokeLater(new Runnable()
@@ -181,8 +186,6 @@ public class AddStudent extends javax.swing.JFrame
             }
         });
     }
-
-    // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel background;
     private javax.swing.JLabel branch;
     private javax.swing.JComboBox<String> branchofstudent;
@@ -199,5 +202,4 @@ public class AddStudent extends javax.swing.JFrame
     private javax.swing.JLabel rollno;
     private javax.swing.JTextField rollnoforstudent;
     private javax.swing.JButton submit;
-    // End of variables declaration//GEN-END:variables
 }
